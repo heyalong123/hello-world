f64bf057a2dfb8892baf64cea938e537e347df9a
hyl <799785959@qq.com>
2020-05-08 12:39:12 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import index from './style/index.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
